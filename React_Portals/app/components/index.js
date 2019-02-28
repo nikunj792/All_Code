502502a14ba9ccadc2bac21cdfe7e1bb72a79c0a
nikunj_792 <nikunjkumar.agarwal@lnttechservices.com>
2019-02-28 10:24:42 +0530

    Changes

--- React_Portals/app/components/index.js
+++ React_Portals/app/components/index.js
@@ -1,24 +1,93 @@
 import React from 'react';
-import {PropTypes} from 'react';
+import { PropTypes } from 'react';
 import FieldSet from 'FieldSet';
-import SetState from 'SetState';
+// import SetState from 'SetState';
 
-export default class Index extends React.Component{
+export default class Index extends React.Component {
 
-	constructor(props){
-		super(props);
-		this.state={
-			
-		}
-	}
-	render(){
-		const arr = ['jack','tom','ella','margretta','jam']
-		return(
-			<div>
-				{'I am index'}
-				<FieldSet name={arr}/>
-                <SetState />
-			</div>
-		);
-	}
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+
+  //   }
+  // }
+
+  // render() {
+  //   const arr = ['jack', 'tom', 'ella', 'margretta', 'jam']
+  //   return (
+  //     <div>
+  //       <a href="#" onClick={this.handleClick.bind(this)}>Login</a>
+  //       {'I am index'}
+  //       <FieldSet name={arr} />
+  //       {/* <SetState /> */}
+  //     </div>
+  //   );
+  // }
+
+  componentDidMount() {
+    document.addEventListener('FBObjectReady', this.initializeFacebookLogin);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('FBObjectReady', this.initializeFacebookLogin);
+  }
+
+  /**
+   * Init FB object and check Facebook Login status
+   */
+  initializeFacebookLogin() {
+    window.FB = window.FB;
+    this.checkLoginStatus();
+  }
+
+  /**
+   * Check login status
+   */
+  checkLoginStatus() {
+    window.FB.getLoginStatus(this.facebookLoginHandler);
+  }
+
+  /**
+   * Check login status and call login api is user is not logged in
+   */
+  facebookLogin() {
+    if (!window.FB) return;
+
+    window.FB.getLoginStatus(response => {
+      if (response.status === 'connected') {
+        this.facebookLoginHandler(response);
+      } else {
+        window.FB.login(this.facebookLoginHandler, { scope: 'public_profile' });
+      }
+    });
+  }
+
+  /**
+   * Handle login response
+   */
+  facebookLoginHandler(response) {
+    if (response.status === 'connected') {
+      window.FB.api('/me', userData => {
+        // let result = {
+        //   ...response,
+        //   user: userData
+        // };
+        debugger;
+        // this.props.onLogin(true, result);
+      });
+    } else {
+      // this.props.onLogin(false);
+    }
+  }
+
+  render() {
+    // let { children } = this.props;
+    return (
+      <div onClick={this.facebookLogin}>
+        {/* {children} */}
+        <p>Hello</p>
+      </div>
+    );
+  }
 }
+
--- React_Portals/server.js
+++ React_Portals/server.js
@@ -5,6 +5,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(3001, function () {
   console.log('Express server is up on port 3000');
 });
